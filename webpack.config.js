593d8fc253399d020bc1df51807d7fb4e730d80f
Margarets00 <minj01134@gmail.com>
2020-11-24 18:19:03 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	// entry: 웹팩에게 어플리케이션이 어디서 시작하고 어디서부터 파일들을 묶을건지 시작점을 정해준다. 
   entry: "./src/index.js",
 	// 현재 개발 모드에서 작업 중임을 알려줌. 
-  mode: "development",
+  mode: "production",
 // export한 JS 모듈이 어떻게 변환되는지 정의한다. 방법은 rules에 정의한 대로 이루어진다. 
   module: {
     rules: [
