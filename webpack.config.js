c681134b5c319753ccc8daa6279cc25eaf7423b9
Margarets00 <minj01134@gmail.com>
2020-11-25 11:43:17 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	// entry: 웹팩에게 어플리케이션이 어디서 시작하고 어디서부터 파일들을 묶을건지 시작점을 정해준다. 
   entry: "./src/index.js",
 	// 현재 개발 모드에서 작업 중임을 알려줌. 
-  mode: "production",
+  mode: "development",
 // export한 JS 모듈이 어떻게 변환되는지 정의한다. 방법은 rules에 정의한 대로 이루어진다. 
   module: {
     rules: [
@@ -28,19 +28,21 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"],fallback: { "path": require.resolve("path-browserify") } },
 // output: 번들링 된 결과물을 어디다 둘 것인지에 대한 설정이 가능.
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "build"),
 	// 번들이 생기는 경로를 지정. webpack-dev-server도 이를 참조
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
 // webpack-dev-server의 옵션을 설정
   devServer: {
     hot :true,
     // 정적 파일 경로 설정
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
+    contentBase: path.join(__dirname, "build/"),
+    //contentBase:"/build",
+    inline:false,
+   port: 3000,
 	// 번들된 코드가 실제로 어디 있는지 서버에게 알려주는 거임
-    publicPath: "http://localhost:3000/dist/",
+    //publicPath: "http://localhost:3000/build/",
 	// devserver 에서만 핫로딩 가능하게
     hotOnly: true
   },
@@ -48,7 +50,7 @@ module.exports = {
 	new webpack.HotModuleReplacementPlugin(),
 	new HtmlWebpackPlugin({
 	// 번들링된 JS를 주입하고 결과물을 옮길 대상이 되는 파일을 지정
-      template: './public/index.html',
+      template: path.resolve('./build/index.html'),
     }),
 	new CleanWebpackPlugin(),
 ]
