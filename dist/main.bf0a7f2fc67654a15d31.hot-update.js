e77393b7a9f59dda28aae36003e2a300e51872c7
margarets00 <minj01134@gmail.com>
2021-01-27 18:59:36 +0900

    fix issu

---
+++ dist/main.bf0a7f2fc67654a15d31.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatetf_ssurank_web_layout"]("main",{
+
+/***/ "./src/components/CommentList.jsx":
+/*!****************************************!*\
+  !*** ./src/components/CommentList.jsx ***!
+  \****************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/margarets/Dev_margaret/tf_SSURANK/src/components/CommentList.jsx: Unexpected token (9:4)\n\n\u001b[0m \u001b[90m  7 | \u001b[39m        content\u001b[33m:\u001b[39m\u001b[32m'한 줄 평 기능 업데이트를 위해 열심히 준비중입니다. 위 버튼을 눌러 업데이트 알림을 신청해주세요!'\u001b[39m\u001b[33m,\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  8 | \u001b[39m        createdAt\u001b[33m:\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  9 | \u001b[39m    }]\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 10 | \u001b[39m    \u001b[36mconst\u001b[39m [recent\u001b[33m,\u001b[39msetRecent]\u001b[33m=\u001b[39museState(\u001b[36mtrue\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 11 | \u001b[39m    \u001b[36mreturn\u001b[39m(\u001b[0m\n\u001b[0m \u001b[90m 12 | \u001b[39m        \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m className\u001b[33m=\u001b[39m\u001b[32m\"detail-comment-list\"\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:748:17)\n    at Object.raiseWithData (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:741:17)\n    at Object.raise (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:735:17)\n    at Object.unexpected (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9101:16)\n    at Object.parseExprAtom (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:10575:20)\n    at Object.parseExprAtom (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:4763:20)\n    at Object.parseExprSubscripts (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:10150:23)\n    at Object.parseUpdate (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:10130:21)\n    at Object.parseMaybeUnary (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:10119:17)\n    at Object.parseExprOps (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9989:23)\n    at Object.parseMaybeConditional (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9963:23)\n    at Object.parseMaybeAssign (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9926:21)\n    at /Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9893:39\n    at Object.allowInAnd (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:11547:12)\n    at Object.parseMaybeAssignAllowIn (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:9893:17)\n    at Object.parseObjectProperty (/Users/margarets/Dev_margaret/tf_SSURANK/node_modules/@babel/parser/lib/index.js:11092:101)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "7d977cf31cf1ff415841"
+/******/ 	})();
+/******/ 	
+/******/ }
+);
