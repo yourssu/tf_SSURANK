36349228d27cd55ad5fe402407150954e1cb10f0
margarets00 <minj01134@gmail.com>
2021-04-05 21:30:50 +0900

    edit many things

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
@@ -10,14 +10,20 @@ const SearchBox = ({history,match,category})=>{
     function search(e){
         setValue(e.target.value);
     }
+    function decodeKeyword(){
+        return(decodeURIComponent(value))
+    }
     function enterSubmit(e){
         if (e.keyCode === 13&&value.length>0) {
             history.push((category===1?'/class':'/professor')+`/search/${encodeURIComponent(value.replace(/ /g,""))}`);
         }
     }
+    useEffect(() => {
+    }, [value])
     return(
     <div className="search bs pd-16-side">
-    <input className="search_bar" value={decodeURI(value)} onChange={search} onKeyDown={(e) => enterSubmit(e) } type="search" placeholder={placeholder}/>
+        {console.log(value)}
+    <input className="search_bar" value={decodeKeyword()} onChange={search} onKeyDown={(e) => enterSubmit(e) } type="search" placeholder={placeholder}/>
     <Link to={()=>value.length>0&&((category===1?'/class':'/professor')+`/search/${encodeURIComponent(value.replace(/ /g,""))}`)}><div className="search_bar_icon"><img src="/img/search_Icon.svg"/></div></Link>
     </div>
     
